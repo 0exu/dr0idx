d366c660fc9e0ded6120ce1dc0b78dc5de53e99e
ZERO WARN <official0warn@gmail.com>
2026-09-26 21:11:26 +0530

    (+) 1. Main landing page fixed 2. background added {Main-user-page} [Add & Update]

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,27 @@
-// This is the javascript placeholder. I will help this to manage it. 
+//Creating elements for handling...
+const roleSelector = document.getElementById('roleSelector')
+const submitBtn = document.getElementById('submitBtn')
+
+// This is the main event listner 
+submitBtn.addEventListener('click', function() {
+  const selectedRole = roleSelector.value;
+
+  // Null value throw on him/her face...
+  if (selectedRole == '') {
+    alert('Please select a role to continue...')
+    return;
+  }
+  // main selector...
+  if (selectedRole === 'student') {
+    window.location.href = '../student/index.html';
+  } else if (selectedRole === 'teacher') {
+    window.location.href = '../teacher/index.html';
+  }
+});
+
+// Beauty of javascript. Hit enter after select you will land to that page. 
+roleSelector.addEventListener('keypress', function(event){
+  if(event.key === 'Enter') {
+    submitBtn.click();
+  }
+})
